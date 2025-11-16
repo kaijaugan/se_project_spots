316e2e48c04228862d44fd0b87c3af0772afe728
Kai Jaugan <kaijaugan@gmail.com>
2025-11-15 21:35:03 -0800

    Project Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
+
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostCloseBtn = newPostModal.querySelector(".modal__submit-btn");
 
 editProfileBtn.addEventListener("click", function() {
     editProfileModal.classList.add("modal_is-opened");
